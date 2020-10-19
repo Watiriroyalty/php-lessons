24cce7c8c717c5c2a8cb0273e21c529abda1f6e0
TiriKay <mary.kambo@strathmore.edu>
2020-10-19 11:26:00 +0300

    The very first introduction to PHP Basics

---
+++ LESSON 1: INTRODUCTION/index.php
@@ -0,0 +1,40 @@
+
+<h1>HEY THERE :) THIS IS MY FIRST PHP WEBSITE</h1>
+
+<p>This HTML will get delivered as is</p>
+
+
+
+<?php echo "<p> But this code is interpreted by PHP and turned into HTML </p>"; ?>
+
+<?php echo "<ul><li> You can use any HTML tags, </li><li> like this list. </li></ul>"; ?>
+
+<footer>
+  <p> And this code is back in plain HTML </p>
+
+  <p> This is an exploration of how PHP can be sent from the back-end to the front-end where it is received as HTML TO be displayed by a browser. </p>
+
+
+</footer>
+
+<?php
+//This is PHP script about to ignore the whitespace and run smoothly
+#This is also another way of having a single line comment in PHP.
+echo                                     "I looooooooveeee PHP so damn much";
+
+/*This is a multi-line comment
+we gonna be commenting
+several lines of code at
+once */
+
+/* "I've never thought of PHP as more
+than a simple tool to solve problems."
+- Rasmus Lerdorf */
+?>
+
+
+
+<?php
+
+#This PHP script does not require a closing tag.....it still runs well
+ echo "I looooooooveeee PHP";
